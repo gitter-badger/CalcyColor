1e30cc91be61b7463813a169ad58f753bb01d1b8
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-05-25 23:37:57 +0200

    Update calcy.py

--- calcy.py
+++ calcy.py
@@ -3,33 +3,33 @@ from termcolor import colored
 def calcy():
     print (colored("Calcy 1.0","blue"))
     print (colored("Type 'help' to view a list of commands.","blue"))
-    user_data = raw_input(colored("$>>", "cyan"))
+    user_data = raw_input(colored("$>> ", "cyan"))
     if user_data == "help":
         help.help()
     elif user_data == "add":
         a = (colored("Type a value here: ", "magenta"))
         b = (colored("Type a value here: ", "magenta"))
-        c = float(a) + float(b)
+        c = a + b
         d = str(c)
         a = (colored((d), "green"))
     elif user_data == "exit":
-        print (colored("Exiting...", "red"))
+        print (colored("Exiting...", "yellow"))
         exit()
     else:
         print (colored("Error: Wrong command", "red"))
         calcy.calcy()
     while user_data == "add" or user_data == "help":
-        user_data = raw_input(colored("$>>", "cyan"))
+        user_data = raw_input(colored("$>> ", "cyan"))
         if user_data == "help":
             help.help()
         elif user_data == "add":
             a = (colored("Type a value here: ", "magenta"))
             b = (colored("Type a value here: ", "magenta"))
-            c = float(a) + float(b)
+            c = a + b
             d = str(c)
             a = (colored((d), "green"))
         elif user_data == "exit":
-            print (colored("Exiting...", "red"))
+            print (colored("Exiting...", "yellow"))
             exit()
         else:
             print (colored("Error: Wrong command", "red"))
