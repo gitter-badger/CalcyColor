a84cf3df6d0dfea334294ef4ebca380aebe36163
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-05-26 00:06:49 +0200

    Update help.py

--- help.py
+++ help.py
@@ -1,5 +1,5 @@
 from termcolor import colored
 
 def help():
-    print (colored(" add   adds two numbers", "grey"))
-    print (colored(" exit   exits programme", "grey"))
+    print (colored(" add   adds two numbers", "white"))
+    print (colored(" exit   exits programme", "white"))
