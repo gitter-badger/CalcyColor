ed18392e13752addc5aee2ae87229eb327d84d9d
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-05-25 23:28:44 +0200

    Add files via upload

---
+++ calcy.py
@@ -0,0 +1,36 @@
+from termcolor import colored
+
+def calcy():
+    print (colored("Calcy 1.0","blue"))
+    print (colored("Type 'help' to view a list of commands.","blue"))
+    user_data = raw_input(colored("$>>", "cyan"))
+    if user_data == "help":
+        help.help()
+    elif user_data == "add":
+        a = (colored("Type a value here: ", "magenta"))
+        b = (colored("Type a value here: ", "magenta"))
+        c = float(a) + float(b)
+        d = str(c)
+        a = (colored((d), "green"))
+    elif user_data == "exit":
+        print (colored("Exiting...", "red"))
+        exit()
+    else:
+        print (colored("Error: Wrong command", "red"))
+        calcy.calcy()
+    while user_data == "add" or user_data == "help":
+        user_data = raw_input(colored("$>>", "cyan"))
+        if user_data == "help":
+            help.help()
+        elif user_data == "add":
+            a = (colored("Type a value here: ", "magenta"))
+            b = (colored("Type a value here: ", "magenta"))
+            c = float(a) + float(b)
+            d = str(c)
+            a = (colored((d), "green"))
+        elif user_data == "exit":
+            print (colored("Exiting...", "red"))
+            exit()
+        else:
+            print (colored("Error: Wrong command", "red"))
+            calcy.calcy()
---
+++ help.py
@@ -0,0 +1,7 @@
+from termcolor import colored
+
+def help():
+    print ("")
+    print (colored(" add   adds two numbers", "grey")
+    print ("")
+    print (colored(" exit   exits programme", "grey")
